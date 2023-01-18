e1c3ff9ea42ad84da17d09f20988c1a1bcd2850f
Augustine Sung <aysung8191@gmail.com>
2023-01-18 14:25:16 -0800

    part 1 complete

---
+++ models/flight.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// const destinationSchema = new Schema({
+//     airport: {
+//         type: String,
+//         enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
+//     },
+//     arrival: Date,
+// })
+
+const flightSchema = new Schema({
+    airline:{
+        type:String,
+        enum: ['American', 'Southwest', 'Alaska Airlines', 'United']
+    } ,
+    airport:{
+        type:String,
+        enum: ['AUS','DFW','DEN','LAX','SAN']
+    },
+    // destinations:{
+    //     type:[destinationSchema]
+    // },
+    flightNo:{
+        type:Number,
+        min:10,
+        max:9999
+    },
+    departs:Date
+}, {
+    timestamps:true
+});
+
+module.exports = mongoose.model('Flight', flightSchema);
