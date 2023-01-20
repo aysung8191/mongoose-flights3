86803423d3d129cdc3903250d4e75e0a354279b5
Augustine Sung <aysung8191@gmail.com>
2023-01-20 15:20:47 -0800

    pt 3 attempted but failed

--- models/flight.js
+++ models/flight.js
@@ -1,6 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const ticketSchema = new Schema({
+    seat: {
+        type: String,
+        match: /[A-F][1-9]\d?/
+    },
+    price: {
+        type: Number,
+        min: 0,
+    },
+    flight: {
+        type: mongoose.ObjectId,
+        ref: 'Flight'
+    }
+})
+
 const destinationSchema = new Schema({
     airport: {
         type: String,
@@ -19,6 +34,7 @@ const flightSchema = new Schema({
         enum: ['AUS','DFW','DEN','LAX','SAN']
     },
     destinations:[destinationSchema],
+    tickets:[ticketSchema],
     flightNo:{
         type:Number,
         min:10,
